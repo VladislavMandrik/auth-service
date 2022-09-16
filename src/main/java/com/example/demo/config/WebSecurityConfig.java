577bad816368370f968337249c603db41bb10292
VladislavMandrik <ladvdrikman@gmail.com>
2022-09-16 09:31:59 +0300

    added Spring Security

---
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class WebSecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+        return httpSecurity
+                .csrf().disable()
+                .formLogin().and()
+                .httpBasic().disable()
+                .authorizeExchange()
+                .pathMatchers("/", "/login", "/favicon.ico").permitAll()
+                .pathMatchers("/hello").hasRole("ADMIN")
+                .anyExchange().authenticated()
+                .and()
+                .build();
+    }
+}
---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.User;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository extends ReactiveCrudRepository<User, Long> {
+    Mono<User> findByUsername(String name);
+}
